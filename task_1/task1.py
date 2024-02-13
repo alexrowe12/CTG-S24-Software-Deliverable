8b1f6304094eef8bb20c9420eac1368bf91ef887
Alex Rowe <alexrowe.biz@gmail.com>
2024-02-12 23:33:46 -0600

    progress on task1

--- task_1/task1.py
+++ task_1/task1.py
@@ -1,3 +1,25 @@
 import yfinance as yf
+import csv
 
-msft = yf.ticker("MSFT")
+tickerList = []
+
+# Opens and reads each line of the tickers.txt file, saving to tickerList
+with open("tickers.txt", "r") as tickerListReader:
+    tickerListReader = csv.reader(tickerListReader)
+    for i in tickerListReader:
+        tickerList.append(i)
+
+# Creates a new csv file for each ticker in the tickerList, and writes data to it
+for ticker in tickerList:
+
+    tickerName = (str(ticker))[2:-2]
+    currTickerObj = yf.Ticker(tickerName)
+
+    # Writes to a new CSV file named after the ticker
+    with open(("task_1/data/" + tickerName + ".csv"), "w", newline='') as csvFile:
+        csvWriter = csv.writer(csvFile)
+
+        data = yf.download(ticker)
+
+        csvWriter.writerow(data)
+
