52f6727a43b0b75b4c5d6cc5f6083ca2d1dc3b5c
Alex Rowe <alexrowe.biz@gmail.com>
2024-02-14 00:45:21 -0600

    temporarily aborted writing to a single file, writing to split files in task_1/data

--- task_2/task2.py
+++ task_2/task2.py
@@ -1,5 +1,6 @@
 import yfinance as yf
 import csv
+import pandas as pd
 
 # Creates list of tickers, same as task 1
 tickerList = []
@@ -23,6 +24,7 @@ for ticker in tickerList:
     pmList = []
     tickerName = (str(ticker))[2:-2]
     filePath = "data/" + tickerName + ".csv"
+    fileWritePath = "task_2/data/" + tickerName + ".csv"
 
     # Opens file, sets to read mode
     with open(filePath, "r") as currFile:
@@ -40,3 +42,16 @@ for ticker in tickerList:
                 pm = round(float(((pastPrices[4] - pastPrices[0]) / pastPrices[0]) * 100))
                 pmList.append(pm)
 
+        pd.DataFrame(pmList).to_csv(fileWritePath)
+
+
+    # Attempt to write in the same format as sample-factor.csv, aborted
+    # with open("task_1/priceMomentum.csv", 'w', newline="") as columnWriter:
+    #     writer = csv.writer(columnWriter)
+    #     with open(filePath, 'r') as columnReader:
+    #         reader = csv.reader(columnReader, delimiter=',')
+    #         for row in reader:  
+    #             row[1] = columnReader.readline() 
+    #             writer.writerow(row)
+
+    # TODO: create function for Volume Adjusted Momentum
